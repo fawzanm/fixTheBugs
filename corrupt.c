4080f93bbecbbe77637a15f5678af3f0708ae305
Mohammed Fawsan <fawzanm@gmail.com>
2016-06-14 06:08:10 +0530

    Initial commit

---
+++ corrupt.c
@@ -0,0 +1,22 @@
+/**
+* Author 	: Mohammed Fawzan
+* Problem 	: Synecdoche
+*/
+#include <stdio.h>
+
+/**
+* I am trying to do something with this method. Life is too short... So I'm lazy to explain it. 
+* Can you find a mistake in the code? 
+* Come on! it's just 3 lines of code, you should do better by now!
+* You need a clue? 
+* In the hitch hikers guide to the Galaxy, 42: The answer to life, the universe and everything
+*/
+int main(){
+
+
+	int i;
+	char string[42] = "Brace your self, CO328 project is coming!!";
+	int j;
+
+	return 0;
+}
