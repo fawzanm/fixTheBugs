557e21ee12d82d87acf5e544fc9a638a22f50a7a
Mohammed Fawsan <fawzanm@gmail.com>
2016-06-14 07:31:25 +0530

    Added a new source file with a bug

---
+++ theta.c
@@ -0,0 +1,17 @@
+/**
+* Author 	: Mohammed Fawzan
+* Problem 	: Guilty
+*/
+
+/**
+*	I need a chunk of string for given size,
+*	Am I making a mistake?  You tell me. 
+*/
+char * giveMeSomeMemory ( int size ){
+	char * str ;
+	if (s > 0)
+		str =( char *) malloc ( size );
+	if (s == 1)
+		return NULL ;
+	return ( str );
+}
