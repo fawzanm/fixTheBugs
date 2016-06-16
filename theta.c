b9a5cfd588f1e3a2ed0bda0a49202fac7c9b6048
e12047 <e12047@ce.pdn.ac.lk>
2016-06-17 02:33:37 +0530

    changed theta.c

--- theta.c
+++ theta.c
@@ -7,13 +7,12 @@
 *	I need a chunk of string for given size,
 *	Am I making a mistake?  You tell me. 
 */
-
 #define NULL 0
 
 char * giveMeSomeMemory ( int size ){
 	char * str ;
 	if (size > 0)
-		str =( char *) malloc ( size );
+		str =( char *) malloc ( sizeof(char)*(size +1));//memory should be allocated for the termination character
 	if (size == 1)
 		return NULL ;
 	return ( str );
