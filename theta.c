a65a815dd4f30f62876cbb5eead1c399717c95d7
Udeni Balasooriya <e12026@ce.pdn.ac.lk>
2016-06-17 17:44:31 +0530

    Fixed the bug of theta.c

--- theta.c
+++ theta.c
@@ -5,16 +5,17 @@
 
 /**
 *	I need a chunk of string for given size,
-*	Am I making a mistake?  You tell me. 
+*	Am I making a mistake?  You tell me.
+	Ans:I changed as if else func and import the library 
 */
 
-
+#include <stdio.h>
 
 char * giveMeSomeMemory ( int size ){
-	char * str ;
+	char * str;
 	if (size > 0)
 		str =( char *) malloc ( size );
-	if (size == 1)
-		return NULL ;
+	else
+		return NULL;
 	return ( str );
 }
