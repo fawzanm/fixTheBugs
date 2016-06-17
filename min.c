1853397c34b7eaf174b55378d0305d4c9c2aeae7
Ching Tien Shi <e12320@ce.pdn.ac.lk>
2016-06-17 20:15:08 +0530

    edited 2 lines
    modified version of min.c

--- min.c
+++ min.c
@@ -8,13 +8,14 @@
 * 	@param int* A 	pointeer to an integer array
 *	@param int n 	number of elements in the array
 */
-int getMin(int *Array, int n) {
 
+int getMin(int *Array, int n) {
+int MAXINT;
  //this is a useless comment  
   int currmin = MAXINT;
 
   for (int i=0; i<n; i++)
-    if (Array[i] > currmin)
+    if (Array[i] < currmin)
       currmin = Array[i];
   return currmin;
 
