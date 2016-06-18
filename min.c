fd8d938cd219a2fba71c283e30fb4bbb119f25c8
harithaki <hharithaki@gmail.com>
2016-06-18 17:52:38 +0530

    MAXINT value defined, the algorithem to get the min value changed

--- min.c
+++ min.c
@@ -4,21 +4,21 @@
 */
 
 /**
-*	Find the minimum vlue from a given integer array
+*	Find the minimum value from a given integer array
 * 	@param int* A 	pointeer to an integer array
 *	@param int n 	number of elements in the array
 */
-int getMin(int *Array, int n) {
 
- //this is a useless comment  
+#define MAXINT 256
+
+int getMin(int *Array, int n){
+ 
   int currmin = MAXINT;
+  int i;
 
-  for (int i=0; i<n; i++)
-    if (Array[i] > currmin)
+  for (i=0; i<n; i++)
+    if (Array[i] < currmin)
       currmin = Array[i];
   return currmin;
 
 }
-
-
-
