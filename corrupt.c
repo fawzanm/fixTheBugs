0e81bd5c9c6c533f3b60352b4cb0eccbd4cb3618
harithaki <hharithaki@gmail.com>
2016-06-18 17:34:00 +0530

    The size should be 43

--- corrupt.c
+++ corrupt.c
@@ -4,8 +4,13 @@
 */
 #include <stdio.h>
 
-int main(){
+/**
+It should have 43 with '\0' character, and as the array is initialized
+no need to mention the size in the array.
+**/
 
+int main(){
+	
 	int i;
 	char string[] = "Brace your self, CO328 project is coming!!";
 	int j;
