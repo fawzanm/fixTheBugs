698985184e23a5cd63223c1a69f953c23b6a211d
Chamith Shanaka <e12379@ce.pdn.ac.lk>
2016-06-19 10:15:53 +0530

    include headers and edit the malloc and if function as well

--- theta.c
+++ theta.c
@@ -7,14 +7,16 @@
 *	I need a chunk of string for given size,
 *	Am I making a mistake?  You tell me. 
 */
-
++#include<stdlib.h>
 #define NULL 0
 
 char * giveMeSomeMemory ( int size ){
 	char * str ;
 	if (size > 0)
-		str =( char *) malloc ( size * sizeof(char) );
-	if (size == 1)
+		-str =( char *) malloc ( size );
+		+str =( char *) malloc ( size * sizeof(char) );
+	-if (size == 1)
+	+else
 		return NULL ;
 	return ( str );
 }
