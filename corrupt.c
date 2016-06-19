46e19e2151179bf0869d453c9615deee230f0904
Jayamine Alupotha <jayamine.alupotha@gmail.com>
2016-06-19 12:09:58 +0530

    E12009
    fixed currupt, minand numbers

--- corrupt.c
+++ corrupt.c
@@ -5,18 +5,21 @@
 #include <stdio.h>
 
 /**
-* I am trying to do something with this method. Life is too short... So I'm lazy to explain myself. 
-* Can you find a mistake in the code? 
+* I am trying to do something with this method. Life is too short... So I'm lazy to explain myself.
+* Can you find a mistake in the code?
 * Come on! it's just 3 lines of code, you should do better by now!
-* You need a clue? 
+* You need a clue?
 * In the hitch hikers guide to the Galaxy, 42: The answer to life, the universe and everything
 */
 int main(){
 
 
 	int i;
-	char string[42] = "Brace your self, CO328 project is coming!!";
+	char input[42] = "Brace your self, CO328 project is coming!!";
 	int j;
-
+    char output[20]= "okey. i'll sleep without thinking about it"
 	return 0;
+	printf("instructor:%s\n",input);
+	printf("student:%s\n",output);
+
 }
