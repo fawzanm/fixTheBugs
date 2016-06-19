8ba1bbdfa6f498af8ede1312c1f83cf647dffb0d
Jayamine Alupotha <jayamine.alupotha@gmail.com>
2016-06-19 12:33:58 +0530

    fixed- comments

--- corrupt.c
+++ corrupt.c
@@ -4,12 +4,7 @@
 */
 #include <stdio.h>
 
-/**
-* I am trying to do something with this method. Life is too short... So I'm lazy to explain myself.
-* Can you find a mistake in the code?
-* Come on! it's just 3 lines of code, you should do better by now!
-* You need a clue?
-* In the hitch hikers guide to the Galaxy, 42: The answer to life, the universe and everything
+/**reaction
 */
 int main(){
 
