8b6caa12be90873067222963367a1f6478ce891f
Mayuri Illesinghe <mayuri1992illesinghe@gmail.com>
2016-06-20 23:11:24 +0530

    Fixed the bug of corrupt.c

--- corrupt.c
+++ corrupt.c
@@ -18,5 +18,15 @@ int main(){
 	char string[42] = "Brace your self, CO328 project is coming!!";
 	int j;
 
+	/*
+		According to the output there are no compile errors and we can print the string. 
+	*/
+
+	/*
+		In this code string array is defined for 42 characters.But with the \n character there is already 43 characters for that string.
+		This is a mistake. The stack is created only for 42 characters and the pointer is pointed out the element 42. 
+		When we define j it ignores the \n character and keeps j there. 
+	*/
+
 	return 0;
 }
