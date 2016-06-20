e357b5440ec00d14948ada9b41d4bf0f8309b277
aaa <kampriyadarshani00000@gmail.com>
2016-06-20 22:52:03 +0530

    1st commit

--- corrupt.c
+++ corrupt.c
@@ -15,7 +15,9 @@ int main(){
 
 
 	int i;
-	char string[42] = "Brace your self, CO328 project is coming!!";
+	//this is a char array.the initializing method is incorrect it shoud be {'B','r','a'} like wise
+	//char string[42] = "Brace your self, CO328 project is coming!!";
+	char string[42] = {'B','r','a','c','e',' ','y','o','u','r',' ','s','e','l','f',',',' ','C','O','3','2','8',' ','p','r','o','j','e','c','t','i','s','c','o','m','m','i','n','g','!','!'};
 	int j;
 
 	return 0;
