008c5958879f62823acfef5564d11594f01c86cc
Mayuri Illesinghe <mayuri1992illesinghe@gmail.com>
2016-06-20 23:00:46 +0530

    Fixed the bug of numbers.c

--- numbers.c
+++ numbers.c
@@ -11,8 +11,17 @@
 int main(){
 
 	int x;
-	for(x=43; x<12423; x++);
-  		printf('x = %d \n', x);
+	for(x=43; x<12423; x++)
+  		printf("x = %d \n", x);
 
 	return 0;
 }
+
+/*
+	printf('x = %d \n', x); 
+			This is the bug.  ' ' should be replaced with " " . In this way -> printf("x = %d \n", x);
+
+	for(x=43; x<12423; x++); 
+			In this case it gives only one number. I mean x = 12423 as the output. But the author is asking for some numbers. (more than one number)
+			Therefore the code is modified in this way -> for(x=43; x<12423; x++) 
+*/  
