8993f5a59b47c3200a0e62bcc107b6c544388125
aaa <kampriyadarshani00000@gmail.com>
2016-06-20 22:52:51 +0530

    3rd commit

--- numbers.c
+++ numbers.c
@@ -11,8 +11,17 @@
 int main(){
 
 	int x;
+	
+	/*
 	for(x=43; x<12423; x++);
-  		printf('x = %d \n', x);
-
+  		printf("x = %d \n", x);
+   */
+   
+   //where print only one number.after loop comletly run,then printf statement run
+   //for print some numbers while loop runing printing have to be done.have to remove (;) 
+   
+   for(x=43; x<12423; x++)
+  		printf("x = %d \n", x);
+   
 	return 0;
 }
