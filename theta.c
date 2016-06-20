0b13eb563b2e0f3eaf477528cf40a86272b1c3f0
Mayuri Illesinghe <mayuri1992illesinghe@gmail.com>
2016-06-20 23:14:57 +0530

    Fixed the bug of theta.c

--- theta.c
+++ theta.c
@@ -8,13 +8,19 @@
 *	Am I making a mistake?  You tell me. 
 */
 
-
+#include <stdlib.h>
+//#define NULL 0
 
 char * giveMeSomeMemory ( int size ){
 	char * str ;
 	if (size > 0)
 		str =( char *) malloc ( size );
-	if (size == 1)
+	if (size == 0) //the bug is fixed here. The code " size == 1 " should be modified as " size == 0 "
 		return NULL ;
 	return ( str );
+}
+
+//only to check the functionality
+void main(){
+	
 }
