12bf7e43151fa968d5939440fabe492382b59163
Gihan Bandara Wijesinghe <e12391@ce.pdn.ac.lk>
2016-06-20 22:21:37 +0530

    Finding the minimum

--- min.c
+++ min.c
@@ -8,14 +8,18 @@
 * 	@param int* A 	pointeer to an integer array
 *	@param int n 	number of elements in the array
 */
+#include <limits.h>
+
 int getMin(int *Array, int n) {
 
  //this is a useless comment  
-  int currmin = MAXINT;
+  //int currmin = MAXINT;
+ int currmin = INT_MAX;
 
   for (int i=0; i<n; i++)
-    if (Array[i] > currmin)
-      currmin = Array[i];
+    //if (Array[i] > currmin)
+    if (Array[i] < currmin)
+      	currmin = Array[i];
   return currmin;
 
 }
