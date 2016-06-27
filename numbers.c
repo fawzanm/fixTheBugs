81e4a5cbe5826b3a67eb40fc69fedd15899b1f14
Shyam <shyamnew17@yahoo.com>
2016-06-28 00:00:43 +0530

    Fixed -> changed the '' to ""

--- numbers.c
+++ numbers.c
@@ -6,12 +6,12 @@
 
 /**
 * I want to print some numbers
-* Can U fxi mE? 
+* Can U fix mE? 
 */
 int main(){
 
 	int x;
-	for(x=43; x<12423; x++)
+	for(x = 43; x < 12423; x++)
   		printf("x = %d \n", x);
 
 	return 0;
