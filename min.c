f83ea08e6f8bc10d1b0e8c150dfa6fc95da03d35
Asanka Dhananjaya Kumara <hhasankahh@gmail.com>
2016-07-14 18:47:05 +0530

    Update min.c

--- min.c
+++ min.c
@@ -1,33 +1,22 @@
 /**
-* Author 	: Mohammed Fawzan
-* Problem 	: If Then Else
+* Author : Mohammed Fawzan
+* Problem : If Then Else
 */
-
-#include<stdio.h>
-#define MAXINT 100000;
-
 /**
-*	Find the minimum vlue from a given integer array
-* 	@param int* A 	pointeer to an integer array
-*	@param int n 	number of elements in the array
+ 
+ 
+* Find the minimum vlue from a given integer array
+* @param int* A pointeer to an integer array
+* @param int n number of elements in the array
 */
+
+
 int getMin(int *Array, int n) {
+//this is a useless comment
 
- //this is a useless comment  
-  int currmin = MAXINT;
-
-  for (int i=0; i<n; i++)
-    if (Array[i] < currmin)
-      currmin = Array[i];
-  return currmin;
-
+ int currmin = MAXINT;
+ for (int i=0; i<n; i++)
+ if (Array[i] < currmin)
+ currmin = Array[i];
+ return currmin;
 }
-
-int main(){
-
-	int A[] = {4, 5, 6, 1, 3, 4, 5, 10};
-	int min = getMin(A, 8);
-	printf("min = %d\n", min);
-	return 0;
-}
-
