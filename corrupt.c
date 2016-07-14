da8b6b8dbf90d4c3081a7eeb487083223262cbf3
Asanka Dhananjaya Kumara <hhasankahh@gmail.com>
2016-07-14 18:58:31 +0530

    Update corrupt.c

--- corrupt.c
+++ corrupt.c
@@ -1,21 +1,24 @@
 /**
-* Author 	: Mohammed Fawzan
-* Problem 	: Synecdoche
+* Author : Mohammed Fawzan
+* Problem : Synecdoche
 */
+
 #include <stdio.h>
- /**
-* I am trying to do something with this method. Life is too short... So I'm lazy to explain myself. 
-* Can you find a mistake in the code? 
+#include <string.h>
+
+/**
+* I am trying to do something with this method. Life is too short... So I'm lazy to explain myself.
+* Can you find a mistake in the code?
 * Come on! it's just 3 lines of code, you should do better by now!
-* You need a clue? 
+* You need a clue?
 * In the hitch hikers guide to the Galaxy, 42: The answer to life, the universe and everything
 */
+
 int main(){
+  int i;
+  char string[43] = "Brace your self, CO328 project is coming!!";
+  int j;
+  printf("%d\n",strlen(string));
 
-
-	int i;
-	char string[42] = "Brace your self, CO328 project is coming!!";
-	int j;
-
-	return 0;
+return 0;
 }
